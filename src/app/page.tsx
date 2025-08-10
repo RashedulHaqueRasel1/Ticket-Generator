5fa1acb153c384639594a46373d8a3488a0a2adc
iftykhar <s.m.ifty49@gmail.com>
2025-08-11 05:55:16 +0600

    card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Home from "@/components/Home/Home";
+import Card from "@/components/Card/Card";
 
 export default function FristPage() {
   return (
     <div>
       <Home />
+      <Card />
     </div>
   );
 }
