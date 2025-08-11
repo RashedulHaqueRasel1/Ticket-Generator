c2b5ea6cd30a39b0209a6b0e47efa7f088b3cc32
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-08-11 23:32:04 +0600

    form responsive

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,8 +4,6 @@ import Header from "./Header/Header";
 import { useState } from "react";
 import TicketForm from "../TicketForm/TicketForm";
 
-
-
 type FormData = {
   fullName: string;
   email: string;
@@ -14,32 +12,23 @@ type FormData = {
 };
 
 const Home = () => {
-
-  
-
-   
-   
-
   return (
     <div className="relative">
       <Background />
-      <main className="relative z-10 min-h-screen flex items-start justify-center px-6 py-12">
+      <main className="conatiner mx-auto z-10 min-h-screen flex items-start justify-center ">
         <div className="w-full max-w-3xl">
           <Header
             title="Your Journey to Coding Conf 2025 Starts Here!"
             subtitle="Secure your spot at next year's biggest coding conference."
           />
 
-          <div  className="mt-8 grid grid-cols-1 gap-6">
-            <div className="max-w-lg">
-              <TicketForm  />
+          <div className="mt-8 w-[90%]  md:w-[70%] mx-auto">
+            <div className="">
+              <TicketForm />
             </div>
-
-            
           </div>
         </div>
       </main>
-
     </div>
   );
 };
